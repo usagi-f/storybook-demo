8b4b138f6ef93792fd6161fd63b5aa61431aeccb
usagi.f@gmail.com <usagi.f@gmail.com>
2017-12-06 12:06:42 +0900

    add: click event

--- react/src/button.js
+++ react/src/button.js
@@ -6,9 +6,17 @@ const className = (color) => {
   return `btn ${color}`;
 };
 
+const handleClick = (color) => {
+  console.log(`click: ${color ? color : 'default'}`);
+};
+
 const Button = (props) => {
   return (
-    <button type="button" className={className(props.color)}>
+    <button
+      type="button"
+      className={className(props.color)}
+      onClick={() => handleClick(props.color)}
+    >
       {props.children}
     </button>
   );
