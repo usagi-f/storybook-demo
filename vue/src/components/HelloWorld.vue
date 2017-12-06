ded8054375b124f3b650a6265e3246d55b16b094
usagi.f@gmail.com <usagi.f@gmail.com>
2017-12-06 12:46:04 +0900

    add: vue button

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -17,12 +17,22 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <div class="buttonGroup">
+      <CustomButton>Info Button</CustomButton>
+      <CustomButton color="orange">Warning Button</CustomButton>
+      <CustomButton color="red">Error Button</CustomButton>
+    </div>
   </div>
 </template>
 
 <script>
+import CustomButton from './CustomButton.vue'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    CustomButton
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -46,5 +56,18 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.buttonGroup {
+  width: 300px;
+  margin: auto;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.buttonGroup button {
+  margin-bottom: 20px;
 }
 </style>
